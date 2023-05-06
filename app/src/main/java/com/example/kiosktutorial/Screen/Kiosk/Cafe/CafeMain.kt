e04af129cb75e94778cf00420510089ddf4c127b
김명석 <twinaudtjr@naver.com>
2023-05-06 18:25:09 +0900

    Cafe Main
    Make Cafe Main activity

---
+++ app/src/main/java/com/example/kiosktutorial/Screen/Kiosk/Cafe/CafeMain.kt
@@ -0,0 +1,98 @@
+package com.example.kiosktutorial.Screen.Kiosk
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.kiosktutorial.R
+import com.example.kiosktutorial.Screen.IKiosk
+import com.example.kiosktutorial.Screen.Screen
+import com.example.kiosktutorial.Screen.TutorialStepData
+
+class CafeMain(isTutorial: Boolean,navHostController: NavHostController? = null) : IKiosk(isTutorial) {
+
+    override var tutorialStepDataList: Map<Int, TutorialStepData> =
+        mutableMapOf(
+            0 to TutorialStepData("카페 메인 화면에서는 아무 화면이나\n 눌러 다음으로 진행합니다. "),
+
+        )
+
+    @Composable
+    fun MainAct(navHostController: NavHostController? = null) {
+        val context = LocalContext.current
+        var toast: Toast? = null
+        var paintD = painterResource(id = R.drawable.cafeimg)
+        Column(modifier = Modifier
+            .fillMaxWidth()
+
+            .clickable {
+                navHostController?.navigate(Screen.CafeKiosk.route)
+            }
+            .paint(painterResource(id = R.drawable.cafeimg), contentScale = ContentScale.Crop)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+            ) {
+                Text(
+                    "KT 카페",
+                    color = Color.White,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "아무곳이나 터치하여 \n 진행해 주세요",
+                    color = Color.White,
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCafeMain() {
+    val kioskTest = CafeMain(true,null)
+    kioskTest.Layout{
+        kioskTest.MainAct()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCafeMainWithReal() {
+    val kioskTest = CafeMain(false,null)
+    kioskTest.Layout{
+        kioskTest.MainAct()
+    }
+}
